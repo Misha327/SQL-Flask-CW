03ba8b54017f8902d00025010ab123f9a198db2e
Misha Vasiljevs <41026719+Misha327@users.noreply.github.com>
2021-01-06 14:04:19 +0000

    Add files via upload

---
+++ coursework.py
@@ -0,0 +1,211 @@
+import psycopg2
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+def getConn():
+    pwFile = open("pw.txt", "r")
+    pw = pwFile.read();
+    pwFile.close()
+    connStr = "dbname= 'Coursework' user='postgres' password= " + pw
+    conn = psycopg2.connect(connStr)
+    return conn
+
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+#1
+@app.route('/addCustomer', methods=['POST'])
+def addEmployee():
+    conn = None
+    try:
+        customerid = request.form['customerid']
+        name = request.form['name']
+        email = request.form['email']
+
+        conn = getConn()
+        cur = conn.cursor()
+        cur.execute('SET search_path to coursework')
+        cur.execute('INSERT INTO customer VALUES (%s, %s, %s)', [customerid, name, email])
+        conn.commit()
+
+        return render_template('home.html', msg='Customer Added')
+    except Exception as e:
+        return render_template('home.html', msg='Customer NOT Added ', error=e)
+    finally:
+        if conn:
+            conn.close()
+
+#2
+@app.route('/createTicket', methods=['POST'])
+def createTicket():
+    conn = None
+    try:
+        ticketid = request.form['ticketid']
+        productid = request.form['productid']
+        priority = request.form['priority']
+        problem = request.form['problem']
+        customerid = request.form['customerid']
+
+        conn = getConn()
+        cur = conn.cursor()
+        cur.execute('SET search_path to coursework')
+
+        cur.execute("INSERT INTO ticket (ticketid, problem, priority, customerid, productid) \
+                    VALUES (%s, %s, %s, %s, %s)", [ticketid, problem, priority, customerid, productid])
+        conn.commit()
+        cur.execute('SELECT * FROM ticket WHERE ticketid = %s', [ticketid])
+
+        colNames = [desc[0] for desc in cur.description]
+        data = cur.fetchall()
+
+        return render_template('tables.html', data=data, colNames=colNames, msg="Ticket Created")
+    except Exception as e:
+        return render_template('home.html', error=e)
+    finally:
+        if conn:
+            conn.close()
+
+#3
+@app.route('/updateTicket', methods=['POST'])
+def updateTicket():
+    conn = None
+    try:
+        ticketupdateid = request.form['ticketupdateid']
+        message = request.form['message']
+        ticketid = request.form['ticketid']
+        staffid = request.form['staffid']
+        conn = getConn()
+        cur = conn.cursor()
+        cur.execute('SET search_path TO coursework')
+
+        cur.execute('INSERT INTO ticketupdate (ticketupdateid, message, ticketid, staffid) \
+                    VALUES (%s, %s, %s, %s)', [ticketupdateid, message, ticketid, staffid])
+        conn.commit()
+
+        return render_template('home.html', msg='Ticket Updated')
+    except Exception as e:
+        return render_template('home.html', error=e, msg='Ticket NOT Updated')
+    finally:
+        if conn:
+            conn.close()
+
+#4
+@app.route('/showOpenTickets', methods=['GET'])
+def showOpenTickets():
+    conn = None
+    try:
+        conn = getConn()
+        cur = conn.cursor()
+        cur.execute('SET search_path TO coursework')
+
+        cur.execute("SELECT ticket.ticketid, status, MAX(updatetime) AS lastupdate FROM ticket \
+                    LEFT JOIN ticketupdate ON ticket.ticketid = ticketupdate.ticketid \
+                    WHERE status = 'open' GROUP BY ticket.ticketid, status")
+
+        colNames = [desc[0] for desc in cur.description]
+        data = cur.fetchall()
+
+        return render_template('tables.html', data=data, colNames=colNames, msg='Open Tickets')
+    except Exception as e:
+        return render_template('home.html', error=e)
+    finally:
+        if conn:
+            conn.close()
+
+#5
+@app.route('/closeTicket', methods=['POST'])
+def closeTicket():
+    conn = None
+    try:
+        ticketid = request.form['ticketid']
+        conn = getConn()
+        cur = conn.cursor()
+        cur.execute('SET search_path TO coursework')
+
+        cur.execute("UPDATE ticket SET status = 'closed' WHERE ticketid = %s", [ticketid])
+        conn.commit()
+
+        return render_template('home.html', msg = 'Ticket Closed')
+    except Exception as e:
+        return render_template('home.html', error=e)
+    finally:
+        if conn:
+            conn.close()
+
+#6
+@app.route('/ticketUpdates', methods=['GET'])
+def ticketUpdates():
+    conn = None
+    try:
+        ticketid = request.args['ticketid']
+        conn = getConn()
+        cur = conn.cursor()
+        cur.execute('SET search_path TO coursework')
+
+        cur.execute("SELECT problem AS question, CASE WHEN ticketupdate.staffid IS NULL THEN customer.name \
+                    ELSE staff.name END AS author_name, message, updatetime FROM ticketupdate INNER JOIN \
+                    ticket ON ticketupdate.ticketid = ticket.ticketid INNER JOIN customer \
+                    ON ticket.customerid = customer.customerid \
+                    LEFT JOIN staff ON ticketupdate.staffid = staff.staffid \
+                    WHERE ticketupdate.ticketid = %s ORDER BY updatetime", [ticketid])
+
+        colNames = [desc[0] for desc in cur.description]
+        data = cur.fetchall()
+
+        return render_template('tables.html', data=data, colNames=colNames, msg='Ticket Updates')
+    except Exception as e:
+        return render_template('home.html', error=e)
+    finally:
+        if conn:
+            conn.close()
+
+#7
+@app.route('/ticketReport', methods=['GET'])
+def ticketReport():
+    conn = None
+    try:
+        conn = getConn()
+        cur = conn.cursor()
+        cur.execute('SET search_path TO coursework')
+
+        cur.execute("SELECT ticket.ticketid, COUNT(message) AS num_updates, MIN(updatetime - loggedtime) \
+                    AS first_update, MAX(updatetime - loggedtime) AS last_update FROM ticket \
+                    LEFT JOIN ticketupdate ON ticket.ticketid = ticketupdate.ticketid WHERE status = 'closed' \
+                    GROUP BY ticket.ticketid")
+
+        colNames = [desc[0] for desc in cur.description]
+        data = cur.fetchall()
+
+        return render_template('tables.html', data=data, colNames=colNames, msg='Closed Tickets Report')
+    except Exception as e:
+        return render_template('home.html', error=e)
+    finally:
+        if conn:
+            conn.close()
+
+#8
+@app.route('/removeCustomer', methods=['GET'])
+def removeCustomer():
+    conn = None
+    try:
+        customerid = request.args['customerid']
+        conn = getConn()
+        cur = conn.cursor()
+        cur.execute('SET search_path TO coursework')
+
+        cur.execute("DELETE FROM customer WHERE customerid = %s", [customerid])
+        conn.commit()
+
+        return render_template('home.html', msg='Customer Removed')
+    except Exception as e:
+        return render_template('home.html', error=e, msg='Customer NOT Removed')
+    finally:
+        if conn:
+            conn.close()
+
+if __name__ == "__main__":
+    app.run(debug=True)
